6dd451249dc9abd2681cc7f2398c7d757a9a71ef
1nk0gn1to <1nk0gn1to@mail.ru>
2022-02-05 22:05:35 +0500

    C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Sum(int[] numbers, int initialValue)
+            {
+                int result = initialValue;
+                foreach (var n in numbers)
+                {
+                    result += n;
+                }
+                Console.WriteLine(result);
+            }
+
+            int[] nums = { 1, 2, 3, 4, 5 };
+            Sum(nums, 10);
+
+            // Sum(1, 2, 3, 4);     // так нельзя - нам надо передать массив
+        }
+    }
+}
