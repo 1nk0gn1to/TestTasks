f26b496433ce8bbb0aa6952078714f308873f4a2
1nk0gn1to <98301366+1nk0gn1to@users.noreply.github.com>
2022-09-25 18:59:20 +0500

    Delete WebApplication1 directory

--- WebApplication1/WeatherForecast.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication1
-{
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
-        public Weather Main { get; set; }
-        public string Name { get; set; }
-    }
-}
